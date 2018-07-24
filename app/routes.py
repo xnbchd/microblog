4a9bd9cb3ff26088d2ea795747b098c0a829d1bc
xnbchd <kelveenkipruto@gmail.com>
2018-07-24 16:43:46 +0300

    version 0.02

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Kelvin'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Nairobi!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Movie was cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
