d53ef917807c6413675dc4214679de2a4d3222ae
xnbchd <kelveenkipruto@gmail.com>
2018-07-24 18:18:39 +0300

    version 0.03

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -17,3 +18,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for {}, remeber_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='sign in', form=form)
